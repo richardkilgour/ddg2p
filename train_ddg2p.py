4063193852396b4746fcf5f1df8228bff0385f9d
Richard (Laptop) <richardkilgour@gmail.com>
2024-11-01 22:50:00 +0100

    Experiments defined by their YMAL file

--- train_ddg2p.py
+++ train_ddg2p.py
@@ -1,12 +1,11 @@
+import argparse
 import os
+import sys
 
 import torch
-from torch._C._profiler import ProfilerActivity
+import yaml
 from torch.profiler import profiler
 from torch.utils.data import DataLoader
-
-#from src.config.config import data_config
-from experiments.experiment_1.config import data_config, model_config
 
 from src.data.utils import pad_collate, test_on_subset
 from src.tools.G2pTrainer import G2pTrainer
@@ -32,40 +31,60 @@ profile = False
 
 # Experiment: Architecture, languages, epochs etc
 
+def load_config(config_file):
+    with open(config_file, 'r') as file:
+        config = yaml.safe_load(file)
+    return config
 
-# Load the data: ['Language', 'Ortho', 'Pref', 'Phon']
-# Use dummy data unless we are in load data or all phase
-ipa_data = IpaDataset(data_config['data_path'], data_config['csv_name'],
-                      languages=data_config['languages'], dummy_data=phase not in ['load_data', 'all'],
-                      max_length=124)
+def main():
+    parser = argparse.ArgumentParser(description='Experiment Configurations')
+    parser.add_argument('--config', type=str, required=True, help='Path to config file')
+    args = parser.parse_args()
 
-# Split into train/test/valid
-train_subset, test_subset, valid_subset = torch.utils.data.random_split(ipa_data, [0.6, 0.2, 0.2],
-                                                                        generator=torch.Generator().manual_seed(1))
+    config = load_config(args.config)
 
-train_dataloader = DataLoader(train_subset, batch_size=data_config['batch_size'], shuffle=True, collate_fn=pad_collate)
+    # Define your model based on config
+    params = {'model': config['model']['model'], 'd_model': config['model']['d_model'], 'n_layers': config['model']['n_layers']}
+    model = ddg2pModel(params).to(device)
 
-params = {'model': model_config['model'], 'd_model': model_config['d_model'], 'n_layers': model_config['n_layers']}
-net = ddg2pModel(params).to(device)
+    # Define optimizer
+    optimizer = torch.optim.SGD(model.parameters(), lr=0.001, momentum=0.9)
 
-# If the network exists, load it
-if os.path.isfile(model_config['PATH']):
-    net.load_state_dict(torch.load(model_config['PATH'], weights_only=False))
+    # Add training loop, etc.
+    # Load the data: ['Language', 'Ortho', 'Pref', 'Phon']
+    # Use dummy data unless we are in load data or all phase
+    ipa_data = IpaDataset(config['data']['data_path'], config['data']['csv_name'],
+                          languages=config['data']['languages'], dummy_data=phase not in ['load_data', 'all'],
+                          max_length=124)
+    
+    # Split into train/test/valid
+    train_subset, test_subset, valid_subset = torch.utils.data.random_split(ipa_data, [0.6, 0.2, 0.2],
+                                                                            generator=torch.Generator().manual_seed(1))
+    
+    train_dataloader = DataLoader(train_subset, batch_size=config['data']['batch_size'], shuffle=True, collate_fn=pad_collate)
+    
+    # If the network exists, load it
+    if os.path.isfile(config['model']['PATH']):
+        model.load_state_dict(torch.load(config['model']['PATH'], weights_only=False))
 
-if phase in ['train', 'all']:
-    optimizer = torch.optim.SGD(net.parameters(), lr=0.001, momentum=0.9)
     # TODO: Is this a good use case for a decorator?
     if profile:
+        from torch._C._profiler import ProfilerActivity
         activities = [ProfilerActivity.CPU, ProfilerActivity.CUDA]
         sort_by_keyword = "cuda_time_total"
         with profiler.profile(activities=activities, with_stack=False, profile_memory=True) as prof:
-            trainer = G2pTrainer(net, train_dataloader, optimizer, device, model_config['PATH'], test_subset=test_subset)
-            trainer.train(model_config['max_epochs'])
+            trainer = G2pTrainer(model, train_dataloader, optimizer, device, config['model']['PATH'],
+                                 test_subset=test_subset)
+            trainer.train(config['model']['max_epochs'])
         print(prof.key_averages(group_by_stack_n=5).table(sort_by=sort_by_keyword, row_limit=10))
     else:
-        trainer = G2pTrainer(net, train_dataloader, optimizer, device, model_config['PATH'], test_subset=test_subset)
-        trainer.train(model_config['max_epochs'])
+        trainer = G2pTrainer(model, train_dataloader, optimizer, device, config['model']['PATH'], test_subset=test_subset)
+        trainer.train(config['training']['max_epochs'])
+    
+    print(f'testng on validation set...')
+    correct_language, correct_phoneme, total_PER = test_on_subset(valid_subset, model, device)
+    print(f'{correct_language=}\t{correct_phoneme=}\t{total_PER=}')
 
-print(f'testng on validation set...')
-correct_language, correct_phoneme, total_PER = test_on_subset(valid_subset, net, device)
-print(f'{correct_language=}\t{correct_phoneme=}\t{total_PER=}')
+
+if __name__ == "__main__":
+    main()
