147fae9942ce182caf4c350ae073b719e24724d1
Richard (Laptop) <richardkilgour@gmail.com>
2024-11-04 21:58:51 +0100

    Minor refactoring

--- src/data/utils.py
+++ src/data/utils.py
@@ -1,8 +1,14 @@
 import random
 
-import torch
 from torch.nn.utils.rnn import pad_sequence
 from torchmetrics.text import WordErrorRate
+
+PAD = u'\x00'  # ASCII Null
+BOS = u'\x02'  # ASCII Start of Text
+EOS = u'\x03'  # ASCII End of Text
+FIN = u'\x04'  # ASCII End of Transmission
+SEP = u'\x1d'  # ASCII Group Seperator (Separates language code from phonemes)
+
 
 def pad_collate(batch):
     (xx, yy) = zip(*batch)
@@ -38,55 +44,10 @@ def tensor_to_utf8(tensor):
     return s
 
 
-def byte_array_to_one_hot(z):
-    # OBSOLETE?
-    # TODO: use torch.nn.functional.one_hot
-    def one_hot_encode(byte_array, num_classes=256):
-        one_hot = torch.zeros(len(byte_array), num_classes)
-        for i, byte in enumerate(byte_array):
-            one_hot[i][byte] = 1
-        return one_hot
-
-    # One-hot encode each byte array
-    one_hot = [one_hot_encode(byte_array) for byte_array in z]
-    # Stack tensors to create a batch
-    return torch.stack(one_hot)
-
-
-def encode_and_pad(x_raw, y_raw, pad=0):
-    # OBSOLETE?
-    max_len = 0
-    x = []
-    y = []
-    for x_, y_ in zip(x_raw, y_raw):
-        x_ = x_.encode('utf-8')
-        y_ = y_.encode('utf-8')
-        # x.append(BOS + x_ + EOS + b'en_US' + SEP + y_)
-        x.append(x_)
-        # Y is offset by one, and has no inputs
-        # Does it need to be masked? Yes.
-        # y.append(PAD * (1 + len(x_)) + b'en_US' + SEP + y_ + EOS)
-        y.append(bytearray([e + 1 for e in x_]))
-        max_len = max(max_len, len(x[-1]))
-
-    # Padding
-    x_padded = [z.ljust(max_len, pad) for z in x]
-    y_padded = [z.ljust(max_len, pad) for z in y]
-
-    return x_padded, y_padded
-
-
 def calculate_per(reference, hypothesis):
     ref_words = list(reference)
     hyp_words = list(hypothesis)
     return WordErrorRate()(ref_words, hyp_words).item()
-
-
-PAD = u'\x00'  # ASCII Null
-BOS = u'\x02'  # ASCII Start of Text
-EOS = u'\x03'  # ASCII End of Text
-FIN = u'\x04'  # ASCII End of Transmission
-SEP = u'\x1d'  # ASCII Group Seperator (Separates language code from phonemes)
 
 
 def get_metrics(word, model, device):
@@ -136,4 +97,4 @@ def test_on_subset(subset, model, device):
         correct_language += correct_language_
         correct_phoneme += correct_phoneme_
         total_per += total_per_
-    return correct_language/subset_len, correct_phoneme/subset_len, total_per/subset_len
+    return correct_language / subset_len, correct_phoneme / subset_len, total_per / subset_len
