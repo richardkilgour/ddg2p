b31dc180a4692e5d5cde42e6a09fb57da65548a6
Richard (Laptop) <richardkilgour@gmail.com>
2024-11-12 10:15:42 +0100

    Test after every 3 epochs

--- src/tools/G2pTrainer.py
+++ src/tools/G2pTrainer.py
@@ -1,5 +1,6 @@
 import threading
 import time
+from datetime import datetime
 
 import torch
 from torch import nn
@@ -9,7 +10,8 @@ from src.data.utils import test_on_subset
 
 
 class G2pTrainer:
-    def __init__(self, model: nn.Module, dataloader: DataLoader, optimizer, device, out_path, use_rpc=False, test_subset=None):
+    def __init__(self, model: nn.Module, dataloader: DataLoader, optimizer, device, out_path, use_rpc=False,
+                 test_subset=None):
         self.use_rpc = use_rpc
         self.model = model
         self.dataloader = dataloader
@@ -47,7 +49,7 @@ class G2pTrainer:
                     rem = remote("zeta_1", self._run_batch(source, targets))
                 else:
                     fut = rpc_async("zeta_0", self._run_batch(source, targets))
-                print(f'{epoch=}; {batch_num=}')
+                print(f'{datetime.now().strftime("%d/%m/%Y %H:%M:%S")}\t{epoch=}\t{batch_num=}')
         else:
             for batch_num, (source, targets) in enumerate(self.dataloader):
                 start_time = time.perf_counter()
@@ -55,14 +57,16 @@ class G2pTrainer:
                 elapsed_time = time.perf_counter() - start_time
                 epoch_total_time += elapsed_time
                 if batch_num % 100 == 0:
-                    print(f'{epoch=}\t{batch_num=}\t{(source.shape)=}\t{elapsed_time:.4f}')
+                    timestamp = datetime.now().strftime("%d/%m/%Y %H:%M:%S")
+                    print(f'{timestamp}\t{epoch=}\t{batch_num=}\t{source.shape=}\t{elapsed_time:.4f}')
         print(f'{epoch_total_time=:.4f}')
-        # After an Epoch, evaluate on the test set
+
+    def _test(self):
         start_time = time.perf_counter()
-        print('Testing...')
+        print(f'Testing starts at {datetime.now().strftime("%d/%m/%Y %H:%M:%S")}')
         results = test_on_subset(self.test_subset, self.model, self.device, beam_width=1)
         testing_elapsed_time = time.perf_counter() - start_time
-        print(f'{testing_elapsed_time=:.4f}')
+        print(f'Testing finished at {datetime.now().strftime("%d/%m/%Y %H:%M:%S")}\t{testing_elapsed_time=:.4f}')
         return results
 
     def train(self, max_epochs):
@@ -79,21 +83,23 @@ class G2pTrainer:
 
             worker1.join()
         else:
-            best_test_wer = 1.1 # 110% ensures the first epoch will improve and net will be saved
+            best_test_wer = 1.1  # 110% ensures the first epoch will improve and net will be saved
             best_test_per = 1.1
             no_improvement_count = 0
             for epoch in range(max_epochs):
-                test_ler, test_wer, test_per = self._run_epoch(epoch)
-                print(f'Tested {epoch=}\t{test_ler=}\t{test_wer=}\t{test_per=}')
-                if test_wer < best_test_wer or (test_wer == best_test_wer and test_per < best_test_per):
-                    print(f'New best {test_wer=}')
-                    no_improvement_count = 0
-                    best_test_wer = test_wer
-                    torch.save(self.model.state_dict(), self.out_path)
-                else:
-                    no_improvement_count +=1
-                    print(f'{test_wer=} is not better than {best_test_wer} - {no_improvement_count=}')
-                if no_improvement_count > 2:
-                    print(f'OVERLEARNING??? after {epoch=}. ABORT training')
-                    break
-
+                self._run_epoch(epoch)
+                # After x epochs, evaluate on the test set
+                if self.test_subset and epoch % 3 == 0:
+                    test_ler, test_wer, test_per = self._test()
+                    print(f'Tested {epoch=}\t{test_ler=}\t{test_wer=}\t{test_per=}')
+                    if test_wer < best_test_wer or (test_wer == best_test_wer and test_per < best_test_per):
+                        print(f'New best {test_wer=}')
+                        no_improvement_count = 0
+                        best_test_wer = test_wer
+                        torch.save(self.model.state_dict(), self.out_path)
+                    else:
+                        no_improvement_count += 1
+                        print(f'{test_wer=} is not better than {best_test_wer} - {no_improvement_count=}')
+                    if no_improvement_count > 2:
+                        print(f'OVERLEARNING??? after {epoch=}. ABORT training')
+                        break
